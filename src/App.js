a6be3a4d2a6c0fda4c6724e84f2db2c6e292cc7a
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-09-23 03:11:54 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './reset.css';
 import './materialize.css'
 import './App.css';
+import Favicon from 'react-favicon';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
@@ -10,6 +11,7 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div>
+        <Favicon url="/favicon.ico"/>
         <Header/>
         <Nav/>
         <div className="main">
